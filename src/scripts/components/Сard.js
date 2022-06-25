3b3fb48bacf78c70e645e21bb809f6cecb4fa6da
Aleksandr Ogorodnikov <Ogorodnikov.Aaleksandr@yandex.ru>
2022-06-25 20:52:22 +0500

    + 5. Отображение колличества лайков

--- src/scripts/components/Сard.js
+++ src/scripts/components/Сard.js
@@ -2,9 +2,11 @@ export class Card {
   constructor(cardData, cardTemplateSelector, handleElementImageClick) {
     this._title = cardData.name;
     this._link = cardData.link;
+    this._id = cardData._id;
     this._cardTemplateSelector = cardTemplateSelector;
     this._handleElementImageClick = handleElementImageClick;
     this._like = false;
+    this._likesCounter = cardData.likes.length;
   }
 
   _getTemplate() {
@@ -42,10 +44,12 @@ export class Card {
     this._elementDeleteButton = this._element.querySelector(
       ".card__delete-button"
     );
-
+    this.elementLikesCounter = this._element.querySelector(".card__likes-counter");
+    this._element.id = this._id;
     this._elmentImage.src = this._link;
     this._elmentImage.alt = this._title;
     this._elementTitel.textContent = this._title;
+    this.elementLikesCounter.textContent = this._likesCounter;
 
     this._setEventListeners();
 
